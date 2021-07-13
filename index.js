7eb8a59200eaaecc89ccd6a0a844b08b6523ada2
Farnerud <hperea.alfa@gmail.com>
2021-07-12 20:19:21 -0500

    br-fetching | Missing changes.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const fetchData = async () => {
-  const response = await axios.get("http://www.omdbapi.com/", {
+  const res = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "8da83971",
       i: "tt0118661",
     },
   });
 
-  console.log(response.data);
+  console.log(res.data);
 };
 
 fetchData();
