598dd5b62822219e5455d51256f858b34c582c13
Farnerud <hperea.alfa@gmail.com>
2021-07-21 22:40:17 -0500

    search-input | Add comments.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Fetch Data for API
 const fetchData = async (searchTerm) => {
   const res = await axios.get("http://www.omdbapi.com/", {
     params: {
@@ -9,21 +10,30 @@ const fetchData = async (searchTerm) => {
   return res.data.Search;
 };
 
+// Select input
 const input = document.querySelector("input");
 
+/*
+ * Call the fetch data on user input
+ */
 const onInput = async (event) => {
+  // add movies fetched in a constant
   const movies = await fetchData(event.target.value);
 
+  // Looping through movies data
   for (let movie of movies) {
     const div = document.createElement("div");
 
+    // Render image and title for each movie
     div.innerHTML = `
       <img src="${movie.Poster}" />
       <h1>${movie.Title}</h1>
     `;
 
+    // Add div elements to the target div
     document.querySelector("#target").appendChild(div);
   }
 };
 
+// Call eventListener on input and debounce it.
 input.addEventListener("input", debounce(onInput, 500));
